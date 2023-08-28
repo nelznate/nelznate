45dd8c7d35747dd65e6e53289791928acdaf9706
nelznate <143271961+nelznate@users.noreply.github.com>
2023-08-29 01:57:38 +0300

    Add files via upload

---
+++ data/config.js
@@ -0,0 +1,48 @@
+export const meta = {
+  title: 'Nelson Nathan - Graphic Designer and Web Developer',
+  description: 'Hey, I\'m Nelson. I\'m a graphic designer and web developer providing success with every solution.'
+}
+
+export const hero = {
+  title: "Hey there! I'm Nelson Nathan.",
+  desc: "I'm a graphic designer and web developer providing success with every solution."
+};
+
+export const projects = {
+  title: 'My Links',
+  desc: "Here are links of my portfolio and socials.",
+  projects: [
+    {
+      title: 'Behance',
+      description: 'Nelson Nathan-Portfolio',
+      link: 'https://behance.com/nelznate'
+    },
+    {
+      title: 'GitHub',
+      description: 'Coding stuff!',
+      github: 'github.com/nelznate'
+    },
+    {
+      title: 'Instagram',
+      description: 'Keeping it real!',
+      link: 'https://instagram.com/nelznate'
+    }
+  ]
+};
+
+export const stack = {
+  title: 'My Stack',
+  stack: [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'Illustrator',
+    'Photoshop'
+  ]
+};
+
+export const contact = {
+  title: 'Contact Me',
+  email: 'nelznate@gmail.com'
+  github: 'nelznate'
+};
